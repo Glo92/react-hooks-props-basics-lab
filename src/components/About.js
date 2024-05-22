f579ebd5f329b95c8f30b75f3a92ed7958f3ec00
Gloria Wekura <wekuraglorian07@gmail.com>
2024-05-22 21:43:57 +0300

    submit lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
+function About(props) {
 
-function About() {
+
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2 >About Me</h2>
+    {props.bio && <p>{props.bio}</p>}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links 
+      user={user}
+      links={user.links}
+       github={user.links.github}
+       linkedin={user.links.linkedin}
+      />
     </div>
   );
 }
